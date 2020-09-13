c1e514f037a8f4fddd4891714bcb20c83264cede
ShaoGuanHotel <15602261894@163.com>
2020-09-13 15:10:00 +0800

    测试提交

--- api/assert/index.js
+++ api/assert/index.js
@@ -1,5 +1,5 @@
 const assert = require('assert').strict
-// assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, 3]], 4, 5]);
+// assert.deepEqual([[[1, 2, 3]],  4, 5], [[[1, 2, 3]], 4, 5]);
 try{
   assert.strictEqual(2,'3','aaaaaaaa')
 }catch(error){
